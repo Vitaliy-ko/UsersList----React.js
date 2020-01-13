a89536c3c84957eda95190439d7d287d19b2272f
Vitaliy_Kalenichenko <vitaliy.kalenichenko@gmail.com>
2020-01-13 13:02:32 +0200

    add scss

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import styles from './UsersList.module.css';
+import styles from './UsersList.module.scss';
 import UsersContext from './../../context/UsersContext';
 
 const usersList = props => (
   <UsersContext.Consumer>
-    {({ activeList }) => {
+    {({ activeList, activeListStyle }) => {
       if (!activeList) return null;
       return (
-        <ol className={styles.Users}>
-          {activeList.map(user => (
-            <li key={user.id}>First Name: {user.firstName}</li>
+        <div className={styles.container}>
+
+        <ul className={styles.UsersList}>
+          {activeList.map((user, index) => (
+            <li className={styles.User} key={user.id}>
+              <span className={[styles.User__Number, styles[activeListStyle]].join(' ')}>{index + 1}</span>{' '}
+              {user.firstName + ' ' + user.lastName}
+            </li>
           ))}
-        </ol>
+        </ul>
+        </div>
+
       );
     }}
   </UsersContext.Consumer>
